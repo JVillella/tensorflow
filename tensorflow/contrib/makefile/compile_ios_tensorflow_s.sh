11a5678f15091e67b2c2a764366124b981be6554
Julian Villella <julian.villella@hotmail.com>
2017-08-07 13:51:08 -0400

    Fix iOS-related build scripts

--- tensorflow/contrib/makefile/compile_ios_tensorflow_s.sh
+++ tensorflow/contrib/makefile/compile_ios_tensorflow_s.sh
@@ -31,10 +31,6 @@ function less_than_required_version() {
   )
 }
 
-if [[ -n MACOSX_DEPLOYMENT_TARGET ]]; then
-    export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion)
-fi
-
 ACTUAL_XCODE_VERSION=$(xcodebuild -version | head -n 1 | sed 's/Xcode //')
 REQUIRED_XCODE_VERSION=7.3.0
 if less_than_required_version $ACTUAL_XCODE_VERSION 7 3 0
@@ -47,23 +43,7 @@ GENDIR=tensorflow/contrib/makefile/gen/
 LIBDIR=${GENDIR}lib
 LIB_PREFIX=libtensorflow-core
 
-# make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile \
-# TARGET=IOS IOS_ARCH=ARMV7 LIB_NAME=${LIB_PREFIX}-armv7.a OPTFLAGS="$1"
-# if [ $? -ne 0 ]
-# then
-#   echo "armv7 compilation failed."
-#   exit 1
-# fi
-
-# make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile \
-# TARGET=IOS IOS_ARCH=ARMV7S LIB_NAME=${LIB_PREFIX}-armv7s.a OPTFLAGS="$1"
-# if [ $? -ne 0 ]
-# then
-#   echo "arm7vs compilation failed."
-#   exit 1
-# fi
-
-make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile \
+make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile_ios \
 TARGET=IOS IOS_ARCH=ARM64 LIB_NAME=${LIB_PREFIX}-arm64.a OPTFLAGS="$1"
 if [ $? -ne 0 ]
 then
@@ -71,15 +51,7 @@ then
   exit 1
 fi
 
-# make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile \
-# TARGET=IOS IOS_ARCH=I386 LIB_NAME=${LIB_PREFIX}-i386.a OPTFLAGS="$1"
-# if [ $? -ne 0 ]
-# then
-#   echo "i386 compilation failed."
-#   exit 1
-# fi
-
-# make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile \
+# make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile_ios \
 # TARGET=IOS IOS_ARCH=X86_64 LIB_NAME=${LIB_PREFIX}-x86_64.a OPTFLAGS="$1"
 # if [ $? -ne 0 ]
 # then
@@ -88,12 +60,12 @@ fi
 # fi
 
 # lipo \
-# ${LIBDIR}/ios_ARMV7/${LIB_PREFIX}-armv7.a \
-# ${LIBDIR}/ios_ARMV7S/${LIB_PREFIX}-armv7s.a \
 # ${LIBDIR}/ios_ARM64/${LIB_PREFIX}-arm64.a \
-# ${LIBDIR}/ios_I386/${LIB_PREFIX}-i386.a \
 # ${LIBDIR}/ios_X86_64/${LIB_PREFIX}-x86_64.a \
 # -create \
 # -output ${LIBDIR}/${LIB_PREFIX}.a
 
-lipo ${LIBDIR}/ios_ARM64/${LIB_PREFIX}-arm64.a -create -output ${LIBDIR}/${LIB_PREFIX}.a
+lipo \
+${LIBDIR}/ios_ARM64/${LIB_PREFIX}-arm64.a \
+-create \
+-output ${LIBDIR}/${LIB_PREFIX}.a
